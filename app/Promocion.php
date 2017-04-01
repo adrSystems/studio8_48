119befe38068991ccd54989b1dd4cd95039d37af
DanielLuna26 <danilr2697@gmail.com>
2017-04-01 02:07:56 -0600

    avance_promociones_admin

--- app/Promocion.php
+++ app/Promocion.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Promocion extends Model
 {
     public $timestamps = false;
+    protected $table = "promociones";
     public function servicios(){
       return $this->hasMany('App\Servicio');
     }
--- resources/views/admin/promociones.blade.php
+++ resources/views/admin/promociones.blade.php
@@ -6,6 +6,19 @@
   body{
     background-image: url("{{asset('/img/walls/admin.jpg')}}");
   }
+  .gestion-promociones{
+    margin-top: 100px;
+  }
+  .promociones{
+    background-color: white;
+    border-radius: 4px;
+    color: #1F1F1F;
+    height: 400px;
+  }
+  .promocion{
+    border: 1px solid darkgray;
+    border-radius: 2px;
+  }
 </style>
 @endsection
 @section('body')
@@ -14,8 +27,25 @@
     <div class="gestion-promociones">
       <div class="col-xs-5">
         <div class="promociones">
-          
+          @foreach(App\Promocion::get() as $promocion)
+          <div class="promocion">
+
+          </div>
+          @endforeach
         </div>
+      </div>
+      <div class="col-xs-offset-1 col-xs-6">
+        <form class="horizontal" action="/promocion/agregar" method="post">
+          <div class="form-group">
+            <input type="file" name="cover" value="" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="date" name="fecha_inicio" value="" class="form-control">
+          </div>
+          <div class="form-group">
+
+          </div>
+        </form>
       </div>
     </div>
   </div>
